cdd6fe3ddae9babc48b13ce7f5eb8841eeb60077
assasino195 <90238903+assasino195@users.noreply.github.com>
2021-09-24 15:11:34 +0800

    hospital sys done

---
+++ Week 2/HospitalSys/HospitalSys/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSys
+{
+    class Program
+    {
+        static void print()
+        {
+            List<hospital> hsptlst = new List<hospital>();
+            bool stay = true;
+
+            Console.WriteLine("Welcome to S Hospital");
+            Console.WriteLine();
+            while (stay)
+            {
+                Console.WriteLine("Please Enter Your Name");
+                string patname = Console.ReadLine();
+                Console.WriteLine("Why are you here today?");
+                Console.WriteLine("1. Emergency Svc");
+                Console.WriteLine("2. Gynae");
+                Console.WriteLine("3. Clinical Svc");
+                Console.WriteLine("4. Othorpedic");
+                Console.WriteLine("5. CheckOut");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        {
+                            hospital h1 = new hospital(patname, input, 100);
+                            hsptlst.Add(h1);
+                            Console.WriteLine("Please head over to the emergency department");
+                            break;
+                        }
+                    case "2":
+                        {
+                            hospital h1 = new hospital(patname, input, 50);
+                            hsptlst.Add(h1);
+                            Console.WriteLine("please head over to Gynae");
+                            break;
+                        }
+                    case "3":
+                        {
+                            hospital h1 = new hospital(patname, input, 20);
+                            hsptlst.Add(h1);
+                            Console.WriteLine("please head over to Clinic");
+                            break;
+                        }
+                    case "4":
+                        {
+                            hospital h1 = new hospital(patname, input, 80);
+                            hsptlst.Add(h1);
+                            Console.WriteLine("please head over to Ortho Department");
+                            break;
+                        }
+                    case "5":
+                        {
+                            
+                            foreach (hospital h in hsptlst)
+                            {
+                                if (patname == h.patientname)
+                                {
+                                    Console.WriteLine("Have you collected your medcine? Y/N");
+                                    string k = Console.ReadLine().ToLower();
+                                    if(k=="y")
+                                    {
+                                        Console.WriteLine(h.charges);
+                                        Console.WriteLine("Pay by Cash?");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Please go over and collect your medcine before coming back");
+                                    }
+
+                                    
+                                }
+                                else { Console.WriteLine("no such patient"); }
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            stay = false;
+                            break;
+                        }
+
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            print();
+            
+        }
+    }
+}
---
+++ Week 2/HospitalSys/HospitalSys/hospital.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSys
+{
+    class hospital
+    {
+        public string patientname { get; set; }
+        public string condition { get; set; }
+        public double charges { get; set; }
+        public hospital (string patientname, string condition,double charges)
+        {
+            this.patientname = patientname;
+            this.condition = condition;
+            this.charges = charges;
+        }
+        public void department()
+        {
+            Console.WriteLine($"Please Head over to {condition} Department.");
+        }
+        public void calculatecharges()
+        {
+            Console.WriteLine($"Total charges are $5 + {charges}");
+        }
+        
+    }
+}
