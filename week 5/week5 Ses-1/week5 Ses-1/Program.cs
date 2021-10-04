f5b2742798f81459255f6ac73c5cec051f32c7e1
assasino195 <90238903+assasino195@users.noreply.github.com>
2021-10-04 11:45:36 +0800

    done

--- week 5/week5 Ses-1/week5 Ses-1/Program.cs
+++ week 5/week5 Ses-1/week5 Ses-1/Program.cs
@@ -11,6 +11,20 @@ namespace week5_Ses_1
 
         static void Main(string[] args)
         {
+            Shape s1 = new Shape();
+            s1.send += S1_send;
+            Console.WriteLine("Enter amount of sides");
+            object s = Console.ReadLine();
+            Console.WriteLine("enter total degrees");
+            object d = Console.ReadLine();
+            s1.getinput(s, d);
+            Console.ReadLine();
+           
+        }
+
+        private static void S1_send(string shapes)
+        {
+            Console.WriteLine(shapes);
         }
     }
 }
---
+++ week 5/week5 Ses-1/week5 Ses-1/Shape.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week5_Ses_1
+{
+    class Shape
+    {
+        public delegate void eventhandler(string shapes);
+        public event eventhandler send;
+        int side;
+        double deg;
+        string shape;
+
+        public void getinput(object s,object d)
+        {
+            try
+            {
+                side = int.Parse((string)(s));
+                deg = double.Parse((string)(d));
+                checkdegree();
+                if(send!=null)
+                {
+                    send(shape);
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine($"Argument Null Exception caught: {e.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"FORMAT Eception raised {ex.Message}");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine($"Over flowed {ex.Message}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("EXCEPTION CAUGHT!");
+            }
+
+
+        }
+        private void checkdegree()
+        {
+           
+              switch(side)
+                {
+                    case 1:
+                        {
+                            shape = "Not any shape";
+                            break;
+                        }
+                    case 2:
+                        {
+                            shape = "Not any shape";
+                            break;
+                        }
+                    case 3:
+                        {
+                            if(deg>180)
+                            {
+                                shape = "Not any shape";
+                            }
+                            else
+                            {
+                                shape = "Triangel Shape!";
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            if(deg>360)
+                            {
+                                shape = "Not any shape";
+                            }
+                            else
+                            {
+                                shape = "Rectangle Shape!";
+                            }
+                            break;
+                        }
+                }
+            
+        }
+
+    }
+}
