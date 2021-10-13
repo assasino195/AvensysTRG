e2c20752011c8e1bedb2dbd07519cfa9264c2162
assasino195 <90238903+assasino195@users.noreply.github.com>
2021-10-13 11:00:55 +0800

    d

---
+++ week 6/fileIO/fileIO/filereader.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileIO
+{
+    class filereader
+    {
+        public Dictionary<char, int> dict = new Dictionary<char, int>();
+        public void Write()
+        {
+            Console.WriteLine("Enter file name: ");
+            string filename = Console.ReadLine();
+
+            FileStream fs = new FileStream($"{filename}.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach(var d in dict)
+            {
+                sw.Write($"{d.Key} count: {d.Value}");
+            }
+            //sw.WriteLine("Hello world");
+
+            sw.Close();
+            fs.Close();
+        }
+
+        public void Append()
+        {
+            Console.WriteLine("Enter file name: ");
+            string filename = Console.ReadLine();
+
+            FileStream fs = new FileStream($"{filename}.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.WriteLine("Hello world");
+
+            sw.Close();
+            fs.Close();
+        }
+
+        public void Read()
+        {
+            Console.WriteLine("Enter file name: ");
+            string filename = Console.ReadLine();
+
+            FileStream fs = new FileStream($"{filename}.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadLine();
+           
+            
+
+            while (str != null)
+            {
+                foreach (char a in str)
+                {
+
+
+                    if (dict.ContainsKey(a))
+                    {
+                        foreach (var d in dict)
+                        {
+                            if (d.Key.Equals(a))
+                            {
+                                Console.WriteLine("added "+a);
+                                dict[a]++;
+                                break;
+                            }
+                           
+                        }
+                    }
+                    else
+                    {
+                        dict.Add(a, 1);
+                    }
+                }
+                Console.WriteLine();
+                str = sr.ReadLine();
+            }
+
+            sr.Close();
+            fs.Close();
+        }
+    }
+}
---
+++ week 6/ipaddresscheck/ipaddresscheck/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ipaddresscheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ints = "1234567890";
+            Console.WriteLine("Enter IP Address");
+            string str = Console.ReadLine();
+            string[] splitted = str.Split('.') ;
+            bool ip = false;
+            foreach(string a in splitted)
+            {
+                int count = 0;
+                bool allint = true;
+                foreach(char b in a)
+                {
+
+                    if (allint)
+                    {
+                        count++;
+                        if (b == '1' || b == '2' || b == '3' || b == '4' || b == '5' || b == '6' || b == '7' || b == '8' || b == '9' || b == '0')
+                        {
+                            allint = true;
+                        }
+                        else
+                        {
+                            allint = false;
+                            break;
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not valid ip");
+                        break;
+                    }
+                }
+                if(allint ==false)
+                {
+                    ip = false;
+                    break;
+                }
+
+                if(count<4)
+                {
+                    ip = true;
+                }
+                else
+                {
+                    break;
+                    ip = false;
+                    Console.WriteLine("Not valid IP");
+                }
+            }
+            if(ip)
+            {
+                Console.WriteLine("REAL IP");
+            }
+            else
+            {
+                Console.WriteLine("FAKE IP");
+            }
+            Console.ReadLine();
+        }
+    }
+}
