21f1d12977b70766a3879edaba1e17b51ead5abc
assasino195 <90238903+assasino195@users.noreply.github.com>
2021-10-14 12:34:42 +0800

    d

--- week 6/Reverse a string without bcl/Reverse a string without bcl/Program.cs
+++ week 6/Reverse a string without bcl/Reverse a string without bcl/Program.cs
@@ -12,10 +12,37 @@ namespace Reverse_a_string_without_bcl
         {
             Console.WriteLine("enter string");
             string a = Console.ReadLine();
+            string oout= string.Empty;
+            List<string> list = new List<string>();
             for(int i=a.Length-1;i>=0;i--)
             {
-                Console.Write(a[i]);
+                if (a[i] ==' ')
+                {
+                    list.Add(oout);
+                    //Console.Write(oout);
+                    string tem = " ";
+                    list.Add(tem);
+                    oout = string.Empty;
+                    
+                }
+                else if(i==0)
+                {
+                    oout += a[i];
+                    list.Add(oout);
+                }
+                else
+                {
+                    oout += a[i];
+                }
+                //oout += a[i];
+                //Console.Write(a[i]);
             }
+            for(int i=list.Count-1;i>=0;i--)
+            {
+                Console.Write(list[i]);
+            }
+            Console.WriteLine();
+            //Console.WriteLine(oout);
             Console.ReadLine();
         }
     }
--- week 6/valid string check string/valid string check string/Program.cs
+++ week 6/valid string check string/valid string check string/Program.cs
@@ -10,6 +10,104 @@ namespace valid_string_check_string
     {
         static void Main(string[] args)
         {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+
+            while (true)
+            {
+                Console.WriteLine("enter string");
+                string a = Console.ReadLine();
+                Stack<char> stack = new Stack<char>();
+                bool valid = false;
+                foreach (char c in a)
+                {
+                    if (c == '{' || c == '[' || c == '(')
+                    {
+                        stack.Push(c);
+                    }
+                    else if (c == '}' || c == ']' || c == ')')
+                    {
+                        char tem = stack.Pop();
+                        if (tem == '{' && c == '}')
+                        {
+
+                            valid = true;
+
+
+                        }
+                       
+                        else if (tem == '[' && c == ']')
+                        {
+
+
+                            valid = true;
+                        }
+                        else if (tem == '('&& c == ')')
+                        {
+                            
+                                valid = true;
+                            
+                          
+                        }
+                        else
+                        {
+                            valid = false;
+                            break;
+
+                        }
+                    }
+                }
+                Console.WriteLine(valid);
+                //int curlybracket = 0;
+                //int smileybracket = 0;
+                //int supercurlybracket = 0;
+                //foreach (char c in a)
+                //{
+                //    switch (c)
+                //    {
+                //        case '{':
+                //            {
+                //                supercurlybracket++;
+                //                break;
+                //            }
+                //        case '[':
+                //            {
+                //                smileybracket++;
+                //                break;
+                //            }
+                //        case '(':
+                //            {
+                //                curlybracket++;
+                //                break;
+                //            }
+                //        case '}':
+                //            {
+                //                supercurlybracket--;
+                //                break;
+                //            }
+                //        case ']':
+                //            {
+                //                smileybracket--;
+                //                break;
+                //            }
+                //        case ')':
+                //            {
+                //                curlybracket--;
+                //                break;
+                //            }
+
+                //    }
+                //}
+                //if (curlybracket != 0 || smileybracket != 0 || supercurlybracket != 0)
+                //{
+                //    Console.WriteLine("not a valid string");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Valid string");
+                //}
+
+            }
         }
     }
 }
